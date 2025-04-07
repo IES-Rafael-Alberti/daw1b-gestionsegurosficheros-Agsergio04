bbf2d3b659b37f077a7f0b2ee9d3c0e362aefe0a
Agsergio04 <agsergio@g.educaand.es>
2025-04-07 19:58:47 +0200

    Seguros acabando v2

--- src/main/kotlin/utils/bs.bat
+++ src/main/kotlin/utils/bs.bat
@@ -1,2 +1,2 @@
 @echo off
-java -jar "C:\Users\sergi\Desktop\Trabajo\fp\Primero\Programacion\kotlin\GestionSeguros\src\main\kotlin\utils\GestionSeguros-1.0.jar"
+java -jar "C:\Users\sergi\Desktop\Trabajo\fp\Primero\Programacion\kotlin\GestionSeguros\src\main\kotlin\utilsGestionSeguros-1.0.jar"
